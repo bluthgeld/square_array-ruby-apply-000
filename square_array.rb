909bd5f39dde975dd7b6a671c9559484798d75fe
Robert Pedersen <rob@worldcrimeleague.org>
2019-02-08 20:19:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,9 +3,7 @@ numbers = [1,2,3]
 def square_array(numbers)
   sqnumbers = []
   numbers.each do |x| 
-    z = x*x
-    numbers << z
-    numbers.delete(x)
+    sqnumbers << z
   end
   sqnumbers
 end
